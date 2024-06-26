a05a7f79a0b9a6d110e896e74ddda3a48ff91ed5
jini Shini <q1887156@gmail.com>
2024-06-26 22:04:04 +0900

    feat: 에러 처리 미들웨어 구현

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { SERVER_PORT } from './constants/env.constant.js';
 import { productsRouter } from './routers/products.router.js';
 import { connect } from './schemas/index.js';
+import { errorHandler } from './middlewares/error-handler.middlware.js';
 
 connect();
 
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use('/api', productsRouter);
+
+app.use(errorHandler);
 
 app.listen(SERVER_PORT, () => {
     console.log(`${SERVER_PORT}번 포트에 연결되었습니다.`);
