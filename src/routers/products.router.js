4d84103c8a389f977d7cc31cc69b3ba0916958d3
jini Shini <q1887156@gmail.com>
2024-06-26 19:17:21 +0900

    feat: 상품 수정, 삭제 API 구현
    - resolved #6, resolved #7

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -40,22 +40,56 @@ productsRouter.get('/products/:id', async (req, res, next) => {
 });
 
 // 상품 수정 (Update)
-productsRouter.put('/products/:id', (req, res, next) => {
+productsRouter.put('/products/:id', async (req, res, next) => {
     // 상품 아이디 파싱하기
+    const {id} = req.params;
+
     // 상품 수정 정보 파싱하기
+    const { name, description, status, manager, password } = req.body;
+
     // DB에서 조회하기 (패스워드 포함)
+    const existedProduct = await Product.findById(id, {password: true});
+
     // 비밀번호 일치 여부 확인
+    const isPasswordMatched = password === existedProduct.password;
+    if (!isPasswordMatched) {
+        return res.status(401).json({status: 401, message: "비밀번호가 일치하지 않습니다."})
+    }
+
+    const productInfo = {
+    ...(name && {name}),
+    ...(description && {description}),
+    ...(status && {status}),
+    ...(manager && {manager}),
+    };
+
     // DB에 갱신하기
+    const data = await Product.findByIdAndUpdate(id, productInfo, {new: true});
+
     // 완료 메시지 반환하기
+    return res.status(200).json({status: 200, message: "상품 수정에 성공했습니다.", data});
 });
 
 // 상품 삭제 (Delete)
-productsRouter.delete('/products/:id', (req, res, next) => {
-    // 상품 아이디 파싱하기
+productsRouter.delete('/products/:id', async (req, res, next) => {
+    // 상품 아이디, 비밀번호 파싱하기
+    const {id} = req.params;
+    const {password} = req.body;
+
     // DB에서 조회하기 (패스워드 포함)
+    const existedProduct = await Product.findById(id, {password: true});
+
     // 비밀번호 일치 여부 확인
+    const isPasswordMatched = password === existedProduct.password;
+    if (!isPasswordMatched) {
+        return res.status(401).json({status: 401, message: "비밀번호가 일치하지 않습니다."})
+    }
+
     // DB에 삭제하기
+    const data = await Product.findByIdAndDelete(id);
+
     // 완료 메시지 반환하기
+    return res.status(200).json({status: 200, message: "상품 삭제에 성공했습니다.", data});
 });
 
 export { productsRouter };
